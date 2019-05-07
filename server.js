cc2159ed934ed71cb9f3ba208a43719cd82e0a10
Joerg Bastian <joerg.bastian@sap.com>
2019-05-08 00:08:09 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const BodyParser = require('koa-bodyparser');
+const Request = require('request-promise-native');
+
+const app = new Koa();
+const router = new Router();
+
+const defaultLanguageCode = "de";
+
+router.get("/probe", ctx => ctx.status = 200); // Liveness / Readiness Probe
+
+router.post("/v1/events/address-update", ctx => {
+    let event = ctx.request.body.event;
+    if (event && event.address && event.address.id && event.address.country) {
+        let originalCountryName = event.address.country;
+        getCanonicalCountryName(originalCountryName).then((canonicalCountryName) => {
+            if (canonicalCountryName !== originalCountryName) {
+                console.log(canonicalCountryName);
+            }
+        });
+        ctx.status = 200; // Success Status Code
+        return;
+    }
+    ctx.status = 400; // Bad Request Status Code
+});
+
+app.use(BodyParser()).use(router.routes()).use(router.allowedMethods());
+const server = app.listen(parseInt(process.env.PORT));
+
+process.on("SIGTERM", () => server.close()); // Stop Server if SIGTERM signal arrives
+
+async function getCanonicalCountryName(countryName) {
+    let countryUppercaseName = countryName.toUpperCase();
+    let countryList = await Request.get(process.env.COUNTRYLIST_ENDPOINT, {json: true});
+    
+    let matchingCountries = countryList.filter((country) => {
+        if (country.name.toUpperCase() === countryUppercaseName)
+            return true;
+
+        if (country.altSpellings.some((countrySpelling) => {
+            if (countrySpelling.toUpperCase() === countryUppercaseName)
+                return true;
+        }))
+            return true;
+
+        if (Object.keys(country.translations).some((translationKey) => {
+            let translation = country.translations[translationKey];
+            if (translation && translation.toUpperCase() === countryUppercaseName)
+                return true;
+        }))
+            return true;
+    });
+
+    if (matchingCountries.length === 1) {
+        return matchingCountries[0].translations[defaultLanguageCode] || matchingCountries[0].name;
+    }
+    return countryName;
+}
